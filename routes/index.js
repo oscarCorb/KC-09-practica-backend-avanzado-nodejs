a365aaa08686d28d1e0df0a70758c631137a5d0f
oscarCorb <info@oscarcorb.com>
2021-02-05 18:34:54 +0100

    ADD: List ejs created to display products in HTML

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const { locals } = require('../app');
+const Product = require('../models/Product');
 const router = express.Router();
 
-// voy a hacerla y luego mira el del profe
+// voy a hacerla y luego comparo con la del del profe
 router.get('/', (req, res, next) => {
     res.render('index');
-    // console.log('test');
-    // res.send('test');
+});
+
+router.get('/products', async (req, res, next) => {
+    // esto es un array de objetos:
+    res.locals.products = await Product.find();
+
+    // res.json(products);
+    res.render('list');
 });
 
 // creo que también haría falta la de productos? o esa va en otro módudlo??? ver archivos agentes.js
